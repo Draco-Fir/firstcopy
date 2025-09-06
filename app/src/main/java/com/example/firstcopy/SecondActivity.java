5cea88847aeadb80e9fc355b4a40867622d37bb7
Prithish <prithishpaulnesaraja@gmail.com>
2025-09-06 19:28:55 -0400

    UPDATED

--- app/src/main/java/com/example/firstcopy/SecondActivity.java
+++ app/src/main/java/com/example/firstcopy/SecondActivity.java
@@ -11,7 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import android.content.Intent;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +20,21 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
-
-        }
+        ImageView biggoal = findViewById(R.id.imageView);
+        ImageView firoption = findViewById(R.id.imageView1);
+        ImageView secoption = findViewById(R.id.imageView2);
+        ImageView thiroption = findViewById(R.id.imageView3);
+        biggoal.setOnClickListener(v -> {
+            Toast.makeText(this, "big goal", Toast.LENGTH_SHORT).show();
+        });
+        firoption.setOnClickListener(v -> {
+            Toast.makeText(this, "first option", Toast.LENGTH_SHORT).show();
+        });
+        secoption.setOnClickListener(v -> {
+            Toast.makeText(this, "second option", Toast.LENGTH_SHORT).show();
+        });
+        thiroption.setOnClickListener(v -> {
+            Toast.makeText(this, "third option", Toast.LENGTH_SHORT).show();
+        });
     }
+}
