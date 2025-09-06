ef18322cbd94915bced41ae5209d03796b8977b0
Prithish <prithishpaulnesaraja@gmail.com>
2025-09-06 13:13:55 -0400

    UPDATED

---
+++ app/src/main/java/com/example/firstcopy/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.firstcopy;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        }
+    }
